d80c24451cceeb8ed8d5d8dd2dd9b8d0325fa061
Panjamapong Sermsawatsri <panjamapong_s@cleverse.com>
2018-12-13 13:46:25 +0700

    refactor: use map from state

--- src/App.js
+++ src/App.js
@@ -2,27 +2,19 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    board: [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+  };
   render() {
     return (
       <div className="App">
         <div className="board">
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
+          {this.state.board.map(row =>
+            row.map(cell => <div className="cell">{cell}</div>)
+          )}
         </div>
+        {this.state.error && <p>Error!</p>}
+        <p>{this.state.message}</p>
       </div>
     );
   }
