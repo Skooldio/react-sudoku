c06f905d748a70b7e1988549364f58deaa898fed
Panjamapong Sermsawatsri <panjamapong_s@cleverse.com>
2018-12-16 00:17:56 +0700

    add board checking and style complete message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,37 @@ class App extends Component {
     invalid: false
   };
   handleClick = (i, j, number) => {
-    console.log(i, j, number);
+    const { board } = this.state;
+    board[i][j] = (board[i][j] + 1) % 5;
+    this.setState({ board });
+  };
+  submit = () => {
+    const { board } = this.state;
+    let isValid = true;
+    for (let i = 0; i < 4; i++) {
+      const horizontal = new Set();
+      const vertical = new Set();
+      const square = new Set();
+      for (let j = 0; j < 4; j++) {
+        horizontal.add(board[i][j]);
+        vertical.add(board[j][i]);
+        square.add(
+          board[2 * (i % 2) + (j % 2)][
+            2 * Math.floor(i / 2) + Math.floor(j / 2)
+          ]
+        );
+      }
+      horizontal.delete(0);
+      vertical.delete(0);
+      square.delete(0);
+      if (horizontal.size !== 4 || vertical.size !== 4 || square.size !== 4) {
+        isValid = false;
+      }
+    }
+    this.setState({
+      invalid: !isValid,
+      complete: isValid
+    });
   };
   render() {
     return (
@@ -32,8 +62,13 @@ class App extends Component {
             ))
           )}
         </div>
-        {this.state.complete && <p>Board is complete!</p>}
-        {this.state.invalid && <p>Board is invalid!</p>}
+        <button onClick={this.submit}>Submit</button>
+        {this.state.complete && (
+          <p style={{ color: "green" }}>Board is complete!</p>
+        )}
+        {this.state.invalid && (
+          <p style={{ color: "red" }}>Board is invalid!</p>
+        )}
       </div>
     );
   }
